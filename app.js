185c6b8c7af29d2cd62b1e6f7e70b3c9874bff45
Bino26 <hbino1999@gmail.com>
2023-10-01 22:49:43 +0000

    Answers for lesson 12

--- app.js
+++ app.js
@@ -32,10 +32,15 @@ app.use(
 );
 //Third-Party Middleware
 app.use(express.json());
+app.use(express.static("public")); // use static html file
 // extra security packages
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get("/", (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
 
 // routes
 app.use("/api/v1/auth", authRouter);
